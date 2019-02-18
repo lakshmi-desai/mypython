6df8c73978459292a7f1c258dfb86da841c2cd05
lakshmi-desai <46952688+lakshmi-desai@users.noreply.github.com>
2019-02-18 20:48:33 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 20:44:33 2019
+
+@author: Lakshmi Desai
+"""
+
+# Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+
+name=input("Enter your name: ")
+output="Hello "+name
+print(output)
+    
