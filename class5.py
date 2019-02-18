9511abfc2f8293972da93838ff72a5dfa0e89c90
lakshmi-desai <46952688+lakshmi-desai@users.noreply.github.com>
2019-02-18 22:35:28 +0530

    Add files via upload

--- class5.py
+++ class5.py
@@ -6,13 +6,15 @@ Created on Mon Feb 18 22:14:36 2019
 """
 
 def pay():
-    h=int(input("Enter hours : "))
-    r=int(input("Enter rate : "))
+       
+    try:
+        h=int(input("Enter hours : "))
+        r=int(input("Enter rate : "))
+        if(h<=40):
+            print(h*r)
+        else:
+            print((40*r)+(h-40)*1.5*r)
+    except:
+        print("Error, please enter numeric value")        
     
-    if(h<=40):
-        return h*r
-    else:
-        return ((40*r)+(h-40)*1.5*r)
-    
-p=pay()
-print(p)
+pay()
