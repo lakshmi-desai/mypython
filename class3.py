fd641be3e3edc55d040f64df42435c066f2f9e85
lakshmi-desai <46952688+lakshmi-desai@users.noreply.github.com>
2019-02-18 20:51:54 +0530

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 20:49:06 2019
+
+@author: Lakshmi Desai
+"""
+
+# Write a program to prompt the user for hours and rate per hour to compute gross pay.
+
+h=int(input("Enter hours: "))
+r=float(input("Enter rate: "))
+pay=h*r
+print(pay)
